c2473e44d7817ff50858e401a2188f94c5ff9e53
LightSage <lightsage01@gmail.com>
2022-07-18 21:38:43 -0500

    Add config routes

--- sanctum/routers/__init__.py
+++ sanctum/routers/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
-from . import guilds, timers, infractions
+from . import guilds, timers, infractions, config
 
 if TYPE_CHECKING:
     from fastapi import FastAPI
@@ -10,3 +10,4 @@ def setup_routers(app: FastAPI):
     app.include_router(guilds.router)
     app.include_router(timers.router)
     app.include_router(infractions.router)
+    app.include_router(config.router)
---
+++ sanctum/routers/config.py
@@ -0,0 +1,49 @@
+from typing import Optional
+
+from fastapi import APIRouter, Request
+
+from sanctum.errors import NotFound
+
+
+router = APIRouter(prefix="/guilds")
+
+
+@router.get("/{guild_id}/config")
+async def get_bot_config(guild_id: int, request: Request):
+    query = """SELECT * FROM guild_config
+               WHERE guild_id=$1;"""
+    record = await request.app.pool.fetchrow(query, guild_id)
+    if not record:
+        raise NotFound("Guild bot config")
+
+    return record
+
+
+@router.get("/{guild_id}/prefixes")
+async def get_guild_prefixes(guild_id: int, request: Request):
+    query = """SELECT prefixes
+               FROM guild_config
+               WHERE guild_id=$1;
+            """
+    record = await request.app.pool.fetchval(query, guild_id)
+    if not record:
+        raise NotFound("Guild prefixes")
+
+    return record
+
+
+@router.put("/{guild_id}/prefixes")
+async def put_guild_prefixes(guild_id: int, prefixes: Optional[list], request: Request):
+    """Upserts new prefixes"""
+    if not prefixes:
+        query = "UPDATE guild_config SET prefixes = NULL WHERE guild_id=$1;"
+        await request.app.pool.execute(query, guild_id)
+        return {}
+
+    query = """INSERT INTO guild_config (guild_id, prefixes)
+               VALUES ($1, $2::text[]) ON CONFLICT (guild_id)
+               DO UPDATE SET
+                   prefixes = EXCLUDED.prefixes;
+            """
+    await request.app.pool.execute(query, guild_id, list(prefixes))
+    return {}
